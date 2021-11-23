6a2bb3f5068d02ff52e29d84a3cf78a1b9e7dae9
Karbonara <37299335+karbonara@users.noreply.github.com>
2021-11-23 18:54:53 +0300

    test

--- src/view/popup-view.js
+++ src/view/popup-view.js
@@ -9,22 +9,18 @@ export const createPopupFilm = () => (
       <div class="film-details__info-wrap">
         <div class="film-details__poster">
           <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
-
           <p class="film-details__age">18+</p>
         </div>
-
         <div class="film-details__info">
           <div class="film-details__info-head">
             <div class="film-details__title-wrap">
               <h3 class="film-details__title">The Great Flamarion</h3>
               <p class="film-details__title-original">Original: The Great Flamarion</p>
             </div>
-
             <div class="film-details__rating">
               <p class="film-details__total-rating">8.9</p>
             </div>
           </div>
-
           <table class="film-details__table">
             <tr class="film-details__row">
               <td class="film-details__term">Director</td>
@@ -58,24 +54,20 @@ export const createPopupFilm = () => (
                 <span class="film-details__genre">Mystery</span></td>
             </tr>
           </table>
-
           <p class="film-details__film-description">
             The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
           </p>
         </div>
       </div>
-
       <section class="film-details__controls">
         <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
         <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
         <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
       </section>
     </div>
-
     <div class="film-details__bottom-container">
       <section class="film-details__comments-wrap">
         <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
-
         <ul class="film-details__comments-list">
           <li class="film-details__comment">
             <span class="film-details__comment-emoji">
