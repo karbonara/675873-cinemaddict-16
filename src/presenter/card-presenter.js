915da8f8208a398b15d8e4768470d8ad51053e79
Karbonara <37299335+karbonara@users.noreply.github.com>
2021-12-16 00:56:05 +0300

    Добавил функции в render.js

--- src/presenter/card-presenter.js
+++ src/presenter/card-presenter.js
@@ -1,10 +1,10 @@
-// import PopupFilmView from '../view/popup-view.js';
+import PopupFilmView from '../view/popup-view.js';
 import FilmCardView from '../view/film-card-view.js';
 import SortView from '../view/sort-view.js';
 import ShowMoreButtonView from '../view/show-more-view.js';
 import FilmContainerView from '../view/film-view.js';
 import LoadingView from '../view/loading-view.js';
-import { render, RenderPosition } from '../utils/render.js';
+import { render, RenderPosition, replace, remove } from '../utils/render.js';
 
 
 const FILM_CARD_COUNT_PER_STEP = 8;
@@ -12,7 +12,7 @@ const FILM_CARD_COUNT_PER_STEP = 8;
 export default class MovieListPresenter {
   #cardContainer = null;
 
-  #cardComponent = new FilmContainerView();
+  #cardsContainerComponent = new FilmContainerView();
   #sortComponent = new SortView();
   #loadComponent = new LoadingView();
 
@@ -27,48 +27,48 @@ export default class MovieListPresenter {
     // Метод для инициализации (начала работы) модуля,
     // малая часть текущей функции renderBoard в main.js
 
-    render(this.#cardContainer, this.#cardComponent, RenderPosition.BEFOREEND);
-    // this.#renderSort();
+    render(this.#cardContainer, this.#cardsContainerComponent, RenderPosition.BEFOREEND);
+
     this.#renderCards();
-    // this.#renderLoading();
   }
 
   // Сортировка
   #renderSort = () => {
-    render(this.#cardContainer, this.#sortComponent, RenderPosition.BEFOREEND);
+    render(this.#cardsContainerComponent.element.querySelector('.films-list'), this.#sortComponent, RenderPosition.BEFOREBEGIN);
   }
 
   // Создание карточки и попапа (renderCard в main.js)
   #renderCard = (card) => {
     const filmComponent = new FilmCardView(card);
-    // const cardPopupComponent = new PopupFilmView(card);
+    const cardPopupComponent = new PopupFilmView(card);
+    const body = document.body;
 
-    // const body = document.body;
-    // const appendPopup = () => {
-    //   cardListElement.appendChild(cardPopupComponent.element);
-    // };
-    // const removePopup = () => {
-    //   cardListElement.removeChild(cardPopupComponent.element);
-    // };
-    // const onEscKeyDown = (evt) => {
-    //   if (evt.key === 'Escape' || evt.key === 'Esc') {
-    //     evt.preventDefault();
-    //     removePopup();
-    //     body.classList.remove('hide-overflow');
-    //     document.removeEventListener('keydown', onEscKeyDown);
-    //   }
-    // };
-    // filmComponent.cardClickHandler(() => {
-    //   appendPopup();
-    //   body.classList.add('hide-overflow');
-    //   document.addEventListener('keydown', onEscKeyDown);
-    // });
-    // cardPopupComponent.popupCloseHandler(() => {
-    //   document.removeEventListener('keydown', onEscKeyDown);
-    //   removePopup();
-    //   body.classList.remove('hide-overflow');
-    // });
-    render(this.#cardComponent, filmComponent, RenderPosition.BEFOREEND);
+    const appendPopup = () => {
+      replace(cardPopupComponent, filmComponent);
+    };
+    const removePopup = () => {
+      remove(filmComponent, cardPopupComponent);
+    };
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        removePopup();
+        body.classList.remove('hide-overflow');
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+    filmComponent.cardClickHandler(() => {
+      appendPopup();
+      body.classList.add('hide-overflow');
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+    cardPopupComponent.popupCloseHandler(() => {
+      document.removeEventListener('keydown', onEscKeyDown);
+      removePopup();
+      body.classList.remove('hide-overflow');
+    });
+    render(this.#cardsContainerComponent.element.querySelector('.films-list__container'), filmComponent, RenderPosition.BEFOREEND);
+    this.#renderLoading();
   }
 
   // Отрисовка N фильмов (карточек)
@@ -77,12 +77,13 @@ export default class MovieListPresenter {
       .slice(from, to)
       .forEach((card) => this.#renderCard(card));
     this.#renderShowMoreButton();
+    this.#renderSort();
   }
 
   // Заглушка
   #renderLoading = () => {
     if (this.#renderCards.length === 0) {
-      render(this.#cardComponent, this.#loadComponent, RenderPosition.BEFOREEND);
+      render(this.#cardsContainerComponent.element.querySelector('.films-list'), this.#loadComponent, RenderPosition.BEFOREEND);
     }
     // render(this.#cardComponent, this.#loadComponent, RenderPosition.BEFOREEND);
   }
@@ -92,7 +93,7 @@ export default class MovieListPresenter {
     if (this.#cardFilms.length > FILM_CARD_COUNT_PER_STEP) {
       let renderCount = FILM_CARD_COUNT_PER_STEP;
       const loadButton = new ShowMoreButtonView();
-      render(this.#cardContainer, loadButton, RenderPosition.BEFOREEND);
+      render(this.#cardsContainerComponent.element.querySelector('.films-list'), loadButton, RenderPosition.BEFOREEND);
       loadButton.setClickHandler(() => {
         this.#cardFilms
           .slice(renderCount, renderCount + FILM_CARD_COUNT_PER_STEP)
--- src/utils/render.js
+++ src/utils/render.js
@@ -31,3 +31,31 @@ export const createElement = (template) => {
 
   return newElement.firstChild; // 3
 };
+export const replace = (newElement, oldElement) => {
+  if (newElement === null || oldElement === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  const newChild = newElement instanceof AbstractView ? newElement.element : newElement;
+  const oldChild = oldElement instanceof AbstractView ? oldElement.element : oldElement;
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
+
+  parent.appendChild(newChild, oldChild);
+};
+export const remove = (component) => {
+  if (component === null) {
+    return;
+  }
+
+  if (!(component instanceof AbstractView)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.element.remove();
+  component.removeElement();
+};
