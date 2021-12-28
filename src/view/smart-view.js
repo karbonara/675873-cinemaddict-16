00153bfbd9873cd3538da947dbf6d4a26a3d8fc8
Karbonara <37299335+karbonara@users.noreply.github.com>
2021-12-28 22:08:39 +0300

    Добавил SmartView

---
+++ src/view/smart-view.js
@@ -0,0 +1,24 @@
+import AbstractView from './abstract-view.js';
+
+export default class SmartView extends AbstractView {
+
+  updateData = (update) => {
+    if (!update) {
+      return;
+    }
+
+    this._data = { ...this._data, ...update };
+
+    this.updateElement();
+  }
+
+  updateElement = () => {
+    const prevElement = this.element;
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.element;
+
+    parent.replaceChild(newElement, prevElement);
+  }
+}
