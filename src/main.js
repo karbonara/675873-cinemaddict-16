b070dbb50b6a460b4dbfd28fe5443ee09b6d3151
Karbonara <37299335+karbonara@users.noreply.github.com>
2022-01-29 22:06:13 +0300

    Добавил переключение страницы

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import HeaderProfileView from './view/header-profile-view.js';
 import FilmListPresenter from './presenter/film-list-presenter.js';
-import { render, RenderPosition } from './utils/render.js';
+import { remove, render, RenderPosition } from './utils/render.js';
 import FilmModel from './model/film-model.js';
 import FilterModel from './view/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
+import StatisticsView from './view/statistic-view.js';
 import CommentsModel from './model/comments-model.js';
-// import SiteMenuView from './view/site-menu-view.js';
-// import { MenuItem } from './const.js';
+import SiteMenuView from './view/site-menu-view.js';
+import { MenuItem } from './const.js';
 import ApiService from './api-service.js';
 
 const AUTHORIZATION = 'Basic er883jdzbdq';
@@ -15,7 +16,7 @@ const END_POINT = 'https://16.ecmascript.pages.academy/cinemaddict';
 const siteMainElement = document.querySelector('.main');
 const siteNavigationElement = document.querySelector('.header');
 
-// const siteMenuComponent = new SiteMenuView();
+const siteMenuComponent = new SiteMenuView();
 const filmModel = new FilmModel(new ApiService(END_POINT, AUTHORIZATION));
 
 const filterModel = new FilterModel();
@@ -24,19 +25,25 @@ const filterModel = new FilterModel();
 const commentsModel = new CommentsModel(new ApiService(END_POINT, AUTHORIZATION));
 // commentsModel.comments = filmComments;
 
-// const handleSiteMenuClick = (menuItem) => {
-// switch (menuItem) {
-//   case MenuItem.FILM_LISTS:
-
-//     break;
-//   case MenuItem.STATISTIC:
-
-//     break;
-// }
-// };
-
 const filmListPresenter = new FilmListPresenter(siteMainElement, filmModel, filterModel, commentsModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, filmModel);
+
+let statisticsComponent = null;
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.FILM_LISTS:
+      remove(statisticsComponent);
+      filterPresenter.init();
+      filmListPresenter.init();
+      break;
+    case MenuItem.STATISTIC:
+      filterPresenter.destroy();
+      filmListPresenter.destroy();
+      statisticsComponent = new StatisticsView();
+      break;
+  }
+};
 
 filterPresenter.init();
 filmListPresenter.init();
@@ -44,6 +51,5 @@ commentsModel.init();
 
 filmModel.init().finally(() => {
   render(siteNavigationElement, new HeaderProfileView(), RenderPosition.BEFOREEND);
-  // Временно закоментил
-  // siteMenuComponent.setFilterTypeChangeHandler(handleSiteMenuClick);
+  siteMenuComponent.setFilterTypeChangeHandler(handleSiteMenuClick);
 });
